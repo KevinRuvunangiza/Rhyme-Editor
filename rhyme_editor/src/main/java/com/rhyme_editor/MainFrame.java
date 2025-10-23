c71fea481b40911182eb82254412a815c039a7d2
ST10462363(Kevin Ruvunangiza) <165159812+KevinRuvunangiza@users.noreply.github.com>
2025-10-23 14:21:08 +0200

    update

--- rhyme_editor/src/main/java/com/rhyme_editor/MainFrame.java
+++ rhyme_editor/src/main/java/com/rhyme_editor/MainFrame.java
@@ -1,7 +1,22 @@
 package com.rhyme_editor;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
 
 public class MainFrame extends JFrame implements ActionListener {
 
